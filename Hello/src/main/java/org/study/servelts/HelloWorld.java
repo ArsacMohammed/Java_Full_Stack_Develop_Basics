b82248f2e174c9a94d6964fe84b5dfa75fa8a01e
ArsacMohammed <arsacskasha@gmail.com>
2023-09-03 08:43:33 +0530

    created new project Hello for servlets and print Helloworld to the console and to the web browser page

--- Hello/src/main/java/org/study/servelts/HelloWorld.java
+++ Hello/src/main/java/org/study/servelts/HelloWorld.java
@@ -1,22 +1,23 @@
+package org.study.servelts;
 
-import java.io.IOException;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
- * Servlet implementation class hello
+ * Servlet implementation class HelloWorld
  */
-@WebServlet("/hello")
-public class hello extends HttpServlet {
+public class HelloWorld extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+       
     /**
-     * Default constructor. 
+     * @see HttpServlet#HttpServlet()
      */
-    public hello() {
+    public HelloWorld() {
+        super();
         // TODO Auto-generated constructor stub
     }
 
@@ -25,15 +26,9 @@ public class hello extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+//		System.out.println("Hello World"); 
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().println("<h1>Hello world</h1>");
 	}
 
 }
